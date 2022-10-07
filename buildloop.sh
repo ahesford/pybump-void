ad802549886c97b436d6b1801a57e133a370c154
Andrew J. Hesford <ajh@sideband.org>
2022-10-07 13:38:30 -0400

    buildloop.sh: make existing package detection much smarter

--- buildloop.sh
+++ buildloop.sh
@@ -9,14 +9,49 @@
 #
 # or follow the README.
 
+get_local_version() {
+  PKG="${1?must specify a package}"
+  ARCH="${2?must specify a target arch}"
+
+  shift 2
+
+  for REPO in "$@"; do
+    REPO=$(realpath -e "${REPO}") || continue
+    QUERY=( "-R" "--repo=${REPO}" )
+
+    # Figure out whether the desired repository holds the package
+    LOC=$(XBPS_TARGET_ARCH="${ARCH}" xbps-query "${QUERY[@]}" -p repository "${PKG}") || continue
+
+    LOC=$(realpath -e "${LOC}") || continue
+    [ "${LOC}" = "${REPO}" ] || continue
+
+    # Find the pkgver for the package in the repository
+    PKGVER=$(XBPS_TARGET_ARCH="${ARCH}" xbps-query "${QUERY[@]}" -p pkgver "${PKG}") || continue
+
+    if [ -n "${PKGVER}" ]; then
+      echo "${PKGVER}"
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+get_needed_version() {
+  PKG="${1?must specify a package}"
+  PKGVER=$(./xbps-src show -p pkgver "${PKG}" | awk '/pkgver:/{print $2}') || return 1
+
+  [ -n "${PKGVER}" ] || return 1
+  echo "${PKGVER}"
+}
+
 PKGLIST="${1?specify a package list}"
 [ -r "${PKGLIST}" ] || exit 1
 
 : ${REPO:=hostdir/binpkgs/py311}
-: ${REPO_ARCH:=x86_64}
+: ${REPO_NONFREE:=${REPO}/nonfree}
 
-mkdir -p "${REPO}"
-REALREPO=$(realpath -e "${REPO}") || exit 1
+: ${REPO_ARCH:=x86_64}
 
 case "${REPO_ARCH}" in
   i686) ROOT_ARCH="i686" ;;
@@ -35,8 +70,13 @@ esac
 ./xbps-src -m "${MASTERDIR}" bootstrap-update
 
 while read -r pkg; do
-  if repo=$(XBPS_TARGET_ARCH="${REPO_ARCH}" xbps-query -Rp repository --repo=$REPO $pkg); then
-    [ "$repo" = "$REALREPO" ] && echo "SKIPPING $pkg, already exists" && continue
+  # Try to find the package in the local repos
+  if HAVE_PKGVER=$(get_local_version "${pkg}" "${REPO_ARCH}" "${REPO}" "${REPO_NONFREE}"); then
+    if NEED_PKGVER=$(get_needed_version "${pkg}"); then
+      if xbps-uhelper cmpver "${HAVE_PKGVER}" "${NEED_PKGVER}"; then
+        echo "SKIPPING $pkg, VERSION ${NEED_PKGVER} already exists" && continue
+      fi
+    fi
   fi
 
   ./xbps-src -m "${MASTERDIR}" clean
